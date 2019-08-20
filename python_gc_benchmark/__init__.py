64f2234843fa436dcb32271c8a0870834ba9afca
Joannah nanjekye <joannah.nanjekye@ibm.com>
2019-08-20 16:07:15 -0300

    Update more copyright headers

--- python_gc_benchmark/__init__.py
+++ python_gc_benchmark/__init__.py
@@ -1,2 +1,12 @@
-VERSION = (0, 1, 2)
+"""
+ * Copyright (c) 2014, 2019 IBM Corp. and others
+ *
+ * This program and the accompanying materials are made available under
+ * the terms of the Eclipse Public License 2.0 which accompanies this
+ * distribution and is available at https://www.eclipse.org/legal/epl-2.0/
+ * or the Apache License, Version 2.0 which accompanies this distribution and
+ * is available at https://www.apache.org/licenses/LICENSE-2.0.
+ """
+ 
+ VERSION = (0, 1, 2)
 __version__ = '.'.join(map(str, VERSION))
